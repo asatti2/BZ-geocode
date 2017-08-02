0d46ecb16e2bb7e60123e84ce9ceccf36257aeda
Gaurav Sahu <gaurav.sahu@globallogic.com>
2017-08-02 17:26:00 +0530

    Issue Fix if waypoints more than 100,

---
+++ src/main/java/com/ops/dto/SourceDestinationInfo.java
@@ -0,0 +1,20 @@
+package com.ops.dto;
+
+public class SourceDestinationInfo {
+	
+	private String sourceAddress;
+	private String destinationAddress;
+	
+	public String getSourceAddress() {
+		return sourceAddress;
+	}
+	public void setSourceAddress(String sourceAddress) {
+		this.sourceAddress = sourceAddress;
+	}
+	public String getDestinationAddress() {
+		return destinationAddress;
+	}
+	public void setDestinationAddress(String destinationAddress) {
+		this.destinationAddress = destinationAddress;
+	}
+}
